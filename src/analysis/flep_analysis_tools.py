c19c9e8473a6542c6fa0cd7e16a92733bd66a6f0
Dicedead <dicedead@gmail.com>
2023-04-26 23:03:43 +0200

    [flep_analysis] comparative curves for student/PhD/campus prices

--- src/analysis/flep_analysis_tools.py
+++ src/analysis/flep_analysis_tools.py
@@ -2,13 +2,25 @@ import pandas as pd
 
 
 def get_mean_resto_by_day(flep_df, resto_str, filter_student_menus=True, threshold_student_menu=9.5):
-    orni_df = flep_df[flep_df["Resto"] == resto_str]
-    orni_df = orni_df[["Prix etudiant", "Prix doctorant", "Prix campus", "date"]]
-    orni_df.dropna()
+    resto_df = flep_df[flep_df["Resto"] == resto_str]
+    resto_df = resto_df[["Prix etudiant", "Prix doctorant", "Prix campus", "date"]].copy()
+    resto_df.dropna()
 
     if filter_student_menus:
-        orni_df = orni_df[orni_df["Prix etudiant"] <= threshold_student_menu]
+        resto_df = resto_df[resto_df["Prix etudiant"] <= threshold_student_menu]
 
-    orni_df = orni_df.groupby('date', as_index=False).mean()
-    orni_df = orni_df.sort_values('date')
-    return orni_df
+    resto_df = resto_df.groupby('date', as_index=False).mean()
+    resto_df = resto_df.sort_values('date')
+    return resto_df
+
+
+def get_mean_all_restos_by_day(flep_df, filter_student_menus=True, threshold_student_menu=9.5):
+    restos_df = flep_df[["Prix etudiant", "Prix doctorant", "Prix campus", "date"]].copy()
+    restos_df.dropna()
+
+    if filter_student_menus:
+        restos_df = restos_df[restos_df["Prix etudiant"] <= threshold_student_menu]
+
+    restos_df = restos_df.groupby('date', as_index=False).mean()
+    restos_df = restos_df.sort_values('date')
+    return restos_df
